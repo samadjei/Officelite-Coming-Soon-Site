f8314988702e4fdab2f70d0f03ed6819b3a4b132
Samuel Adjei <info.samadjei@gmail.com>
2021-04-10 17:43:46 +0100

    added hero section styling

--- pages/homepage.js
+++ pages/homepage.js
@@ -3,18 +3,17 @@ import { Button } from '../components/button';
 
 const Homepage = () => {
 	return (
-		<main>
+		<main className="main">
 			<div className="hero container">
 				<div className="hero__content">
 					<h1 className="hero--title">A simple solution to complex tasks is coming soon </h1>
 					<p className="hero--para">Say goodbye to inefficient juggling of multiple apps, teams, and projects. Officelite is the new collaboration platform built with an intuitive interface to improve productivity.</p>
-					<Button
-              className="btn hero--btn"
-              buttonStyle="btn--primary"
-              buttonSize="btn--desktop"
-            >
-              Get Started
-            </Button>
+					<Button className="btn hero--btn" buttonStyle="btn--primary" buttonSize="btn--desktop">
+						Get Started
+					</Button>
+				</div>
+				<div className="hero__image">
+					<Image src="/assets/home/illustration-charts.svg" height={531} width={475} alt="Illustration Chart" />
 				</div>
 			</div>
 		</main>
